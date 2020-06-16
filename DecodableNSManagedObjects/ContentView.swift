51cdfd9af463a39156b5e390378a48e3dd7688b3
Adar Hefer <adar.hefer@gmail.com>
2020-06-16 15:21:08 +0300

    Initial Commit

---
+++ DecodableNSManagedObjects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DecodableNSManagedObjects
+//
+//  Created by Adar Hefer on 16/06/2020.
+//  Copyright © 2020 Adar Hefer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
